73aeed6735fecc08c84f3034261b6a87958c0eb8
bLuEsKy140506 <115715458+bLuEsKy140506@users.noreply.github.com>
2023-11-07 22:47:52 +0800

    initial setup

---
+++ semantic-parts/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css";
+
+const HeaderComponent = () => {
+  return (
+    <div className="header-container">
+      <div className="logo-container">
+        <h3>ICON LOGO/PICTURE</h3>
+      </div>
+      <nav className="nav-container flex ">
+        <a href="#about">About</a>
+        <a href="#projects">Project</a>
+        <a href="#contact">Contact Me</a>
+      </nav>
+    </div>
+  );
+};
+
+export default HeaderComponent;
